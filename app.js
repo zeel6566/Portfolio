659abed3bc9981e0c862a149938d86373983386d
Zeel Punjabi <60688488+zeel6566@users.noreply.github.com>
2021-03-10 11:24:10 +0530

    Portfolio
    Personal portfolio website

---
+++ app.js
@@ -0,0 +1,21 @@
+let hm = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hm.addEventListener('click',function function_name(argument) {
+	// body...
+
+	mobileNav.classList.add('open');
+});
+
+
+times.addEventListener('click',function function_name(argument) {
+	// body...
+
+		mobileNav.classList.remove('open');
+
+
+});
+
+
+
